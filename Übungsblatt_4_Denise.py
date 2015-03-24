8c7db6551609744ce85090880702bc6947f65eb2
Alexander Blesius <onkel-pflaume@web.de>
2015-03-24 16:13:57 +0100

    tidied project

--- Übungsblatt_4_Denise.py
+++ Übungsblatt_4_Denise.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 #  Übungsblatt_4_Denise.py
-#
-#  Copyright 2015 Alexander Blesius <onkel-pflaume@web.de>
+#  Copyright 2015 Alexander Blesius <onkel-pflaume@web.de>, Denise Schmidt <denise.schmidt@lehramt.uni-giessen.de>
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -16,16 +17,16 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#
-#
+
+import nltk, re
 
 
-import nltk
-
+# Übung 3
+	# Tokenize and tag the following sentence:
+		# They wind back the clock, while we chase after the wind.
+		# What different pronunciations and parts of speech are involved?
 from nltk import word_tokenize
-
 text = word_tokenize("They wind back the clock, while we chase after the wind.")
-
 nltk.pos_tag(text)
 
 # Output: [('They', 'PRP'), ('wind', 'VBP'), ('back', 'RB'), ('the', 'DT'),
@@ -37,35 +38,49 @@ nltk.pos_tag(text)
 # Bei wind/Nomen wird das i als i ausgesprochen.
 
 #Aufgabe 34
+	# There are 264 distinct words in the Brown Corpus having exactly three possible tags.
+		# 1. Print a table with the integers 1..10 in one column,
+			# and the number of distinct words in the corpus having
+			# 1..10 distinct tags in the other column.
+from nltk.corpus import brown
+
+	# 2. For the word with the greatest number of distinct tags,
+		# print out sentences from the corpus containing the word,
+		# one for each possible tag. (15 Punkte)
 
 
 
-#Aufgabe 5
-# 5.1
+# Übung 5
+	# 1. Write code to produce two trees, one for each reading
+		# of the phrase "old men and women"
+from nltk.tree import Tree
 
-grammar1 = nltk.ContextFreeGrammar.fromstring("""
-PHRASE -> AdjP | NP CON NP
-NP -> Adj N | N
-AdjP -> Adj Obj
-Obj -> N CON N
-Adj -> 'old'
-N -> 'men' | 'women'
-CON -> 'and'
+grammar1 = nltk.CFG.fromstring("""
+	PHRASE -> AdjP | NP CON NP
+	NP -> Adj N | N
+	AdjP -> Adj Obj
+	Obj -> N CON N
+	Adj -> 'old'
+	N -> 'men' | 'women'
+	CON -> 'and'
 """)
 
-sent = [("old"), ("men"), ("and"), ("women")]
+sent1 = [("old"), ("men"), ("and"), ("women")]
 
 rd_parser = nltk.RecursiveDescentParser(grammar1)
 
-for tree in rd_parser.parse(sent):
-	print(tree)
+for tree in rd_parser.parse(sent1):
+    print(tree)
 
 # Lesart 1: old men and old women (das Adjektiv wird auf beide Nomen bezogen und
-# dominiert die Nomen = steht im Baum oben).
+	# dominiert die Nomen = steht im Baum oben).
 # Lesart 2: die Phrase besteht aus zwei "unabhängigen Nomen", nämlich alten Männern
-# und Frauen aller Altersstufen.
+	# und Frauen aller Altersstufen.
 
-# 5.2 Satz (S (NP Mary) (VP (V saw) (NP Bob)))
+# 5.2
+# Encode any of the trees presented in this chapter
+	# as a labeled bracketing and use nltk.Tree() to check
+	# that it is well-formed. Now use draw() to display the tree.
 
 tree1 = nltk.Tree('NP', ['Mary'])
 
@@ -84,31 +99,35 @@ tree5.draw()
 # ODER
 
 VP = Tree('VP', [Tree('V', ['saw']), Tree('NP', ['Bob'])])
-sent = s = Tree('S', [Tree('NP', ['Mary']), VP]) #Möglichkeit, sent + VP
-# in eine Zeile zu bringen?
-print(sent)
-sent.draw()
+sent3 = Tree('S', [Tree('NP', ['Mary']), VP]) #Möglichkeit, sent3 + VP in eine Zeile zu bringen?
+print(sent3)
+sent3.draw()
+
+#alternative version for fewer lines
+sent3 = Tree('S', [Tree('NP', ['Mary']), Tree('VP', [Tree('V', ['saw']), Tree('NP', ['Bob'])])]) #Möglichkeit, sent3 + VP in eine Zeile zu bringen?
+print(sent3)
+sent3.draw()
 
 # 5.3
-grammar2 = nltk.ContextFreeGrammar.fromstring("""
-S -> NP VP | NP VP Time
-NP -> Det N
-VP -> V NP VTemp | V NP TemP
-TemP -> VTemp Day
-Time -> Day
-Det -> 'The' | 'a'
-N -> 'man' | 'woman'
-V -> 'saw'
-VTemp -> 'last'
-Day -> 'Thursday'
+grammar2 = nltk.CFG.fromstring("""
+	S -> NP VP | NP VP Time
+	NP -> Det N
+	VP -> V NP VTemp | V NP TemP
+	TemP -> VTemp Day
+	Time -> Day
+	Det -> 'The' | 'a'
+	N -> 'man' | 'woman'
+	V -> 'saw'
+	VTemp -> 'last'
+	Day -> 'Thursday'
 """)
 
-sent = "The woman saw a man last Thursday".split()
+sent2 = "The woman saw a man last Thursday".split()
 
 rd_parser = nltk.RecursiveDescentParser(grammar2)
 
-for tree in rd_parser.parse(sent):
-	print(tree)
+for tree in rd_parser.parse(sent2):
+    print(tree)
 
 # Lesart 1: Sie sah ihn zuletzt Dienstag.
 # Lesart 2: Sie sah ihn letzten Dienstag.
@@ -118,14 +137,20 @@ for tree in rd_parser.parse(sent):
 #Aufgabe 25
 from nltk import word_tokenize
 
-gutenb1 = open('CountryDoctor_deBalzac.txt')
-text1 = gutenb1.read()
+from urllib import request
+urllist = ["http://www.gutenberg.org/ebooks/1659.txt.utf-8", "http://www.gutenberg.org/ebooks/48371.txt.utf-8", "http://www.gutenberg.org/ebooks/3102.txt.utf-8"]
+textlist = []
+for x in range(0, 3):
+	textlist.append(request.urlopen(urllist[x]).read().decode('utf8'))
 
-gutenb2 = open('ChangedHeart_Fleming.txt')
-text2 = gutenb2.read()
+# gutenb1 = open('CountryDoctor_deBalzac.txt')
+# text1 = gutenb1.read()
 
-gutenb3 = open('HauntedMan_Dickens.txt')
-text3 = gutenb3.read()
+# gutenb2 = open('ChangedHeart_Fleming.txt')
+# text2 = gutenb2.read()
+
+# gutenb3 = open('HauntedMan_Dickens.txt')
+# text3 = gutenb3.read()
 
 textegesamt = text1 + text2 + text3
 
@@ -134,8 +159,8 @@ textegesamtneu = re.sub(r'\n', ' ', textegesamt)
 gesamt = nltk.sent_tokenize(textegesamtneu)
 
 longest_len = max([len(s) for s in gesamt])
-sent = [s for s in gesamt if len(s) == longest_len]
-print (sent)
+sent4 = [s for s in gesamt if len(s) == longest_len]
+print (sent4)
 
 # Output: 'Ever to look beyond the present moment, to foresee the ways of
 #Destiny, to care so little for power that he only retains it because he is
